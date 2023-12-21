4b97c9579fedbf576b91b8144c3981e750fdb74f
Julia Romenia <juliarimeniag1@gmail.com>
2023-12-21 00:09:58 -0300

    Alteraçoes estacionamento

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using DesafioFundamentos.Models;
-
-// Coloca o encoding para UTF8 para exibir acentuação
+﻿// Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
